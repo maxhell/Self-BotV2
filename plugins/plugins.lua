360bbb1bec26f563c8c5b83a6223896f58afb6be
maxhell <masihsell@gmail.com>
2017-03-29 10:47:34 +0430

    plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -68,7 +68,7 @@ local function list_plugins(only_enabled)
      -- text = text..v..'  '..status..'\n'
     end
   end
-  local text = text.."\n_🔃All Plugins Reloaded_\n\n"..nact.." *✔️Plugins Enabled*\n"..nsum.." *📂Plugins Installed*\n\n@BeyondTeam"
+  local text = text.."\n_🔃All Plugins Reloaded_\n\n"..nact.." *✔️Plugins Enabled*\n"..nsum.." *📂Plugins Installed*\n\n@DozChat"
 return text
 end
 
